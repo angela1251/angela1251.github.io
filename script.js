888971a755e6eb4c3b9f8e44e63962e6d3980112
angela1251 <114589835+angela1251@users.noreply.github.com>
2022-12-08 09:04:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ownerName = "Angel Atenco ds"; // update the part BETWEEN the "quotes"
+let ownerName = "Angel Atenco"; // update the part BETWEEN the "quotes"
 let userName = "angela1251"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
